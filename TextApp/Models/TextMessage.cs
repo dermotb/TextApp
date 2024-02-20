2b56020839907985eaec1a8fdc5d8e1da25541d8
Dermot Boyle <dermotb@hotmail.com>
2024-02-20 22:14:17 +0000

    Add project files.

---
+++ TextApp/Models/TextMessage.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SMSGateWay.Models
+{
+    public class TextMessage
+    {
+        [Required]
+        [Phone]
+        public string FromNumber { get; set; }
+
+        [Required]
+        [Phone]
+        public string ToNumber { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(140)]
+        public string Content { get; set; }
+    }
+}
---
+++ TextApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using SMSGateWay.Models;
+using System.Net.Http.Headers;
+
+namespace TextApp
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Task result = SendMessageAsync();
+            result.Wait();
+        }
+
+        static async Task SendMessageAsync()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("http://localhost:5292/");
+
+                client.DefaultRequestHeaders.
+                    Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                TextMessage payload = new TextMessage() { FromNumber = "088-12345667", ToNumber = "5558889", Content = "Message from the client" };
+
+                HttpResponseMessage response = await client.PostAsJsonAsync("api/SMS", payload);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("All good, message sent");
+                }
+                else
+                {
+                    Console.WriteLine(response.StatusCode + " | " + response.ReasonPhrase);
+                }
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
